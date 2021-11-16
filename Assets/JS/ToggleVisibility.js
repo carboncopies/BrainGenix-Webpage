eec6cb81a1a4a3c0bf15828a326dd83539f78e52
tliao <tliao@carboncopies.org>
2021-11-15 17:23:46 -0800

    Update Webpage

---
+++ Assets/JS/ToggleVisibility.js
@@ -0,0 +1,10 @@
+
+// Toggles Menu Visibility
+function toggle_visibility() 
+{
+    var e = document.getElementById('mobile-dropdown-content');
+    if ( e.style.display == 'block' )
+        e.style.display = 'none';
+    else
+        e.style.display = 'block';
+}
